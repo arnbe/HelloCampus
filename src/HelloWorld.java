5dc8f2e8d9cd3495cac5249d46c459f6e577dc3f
campus02 <arnelabegic@gmx.at>
2020-03-05 09:54:18 +0100

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main (String[] arg ) {
+        System.out.println("HelloWorld ");
+        System.out.println("HelloWorld");
+        //System.out.println("I am doing this myself");
+        int i = 2;
+        double d = 1.5;
+        double x = i*d + d;
+        System.out.println("Campus 0"+x);
+        // Campus
+        //HelloWorld
+        //TODO: HelloWorld I have to change this
+    }
+}
